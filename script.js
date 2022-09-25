3c63e20fcdeb27acd7940529baecba23b260116d
Hanbi58 <hanxiaonan2012@gmail.com>
2022-09-24 19:03:17 -0700

    My version, does not add class each scroll action

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-alert('1')
+const nav = document.querySelector(".nav");
+
+const checkStart = nav.offsetHeight + 50;
+const checkEnd = nav.offsetHeight + 100;
+
+window.addEventListener("scroll", () => {
+  classToggler(window.scrollY);
+});
+
+function classToggler(sY) {
+  if (sY > checkStart && sY < checkEnd) {
+    nav.classList.add("light");
+    return;
+  }
+
+  if (sY < checkEnd) {
+    nav.classList.remove("light");
+  }
+}
